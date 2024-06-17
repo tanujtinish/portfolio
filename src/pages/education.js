5052a4f44e3199f9d42ed3abb7487d4ad2bec137
tanujtinish <tanujtinish@gmail.com>
2024-06-16 19:23:03 -0500

    deploy on vercel

--- src/pages/education.js
+++ src/pages/education.js
@@ -2,13 +2,33 @@ import React, { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
 import LiIcon from "../components/LiIcon";
 import Education from "../components/Education";
+import Layout from "@/components/Layout";
+import TransitionEffect from "@/components/TransitionEffect";
+import Head from "next/head";
 
 const EducationPage = () => {
   const ref = useRef(null);
+
   return (
-    <Education
-      styleObj={{ marginTop: "calc(var(--base-font-size) * (32 / 32))" }}
-    />
+    <>
+      <Head>
+        <title>Tanuj Gupta's Portfolio | Education Page</title>
+        <meta
+          name="description"
+          content="Welcome to the portfolio of Tanuj Gupta, a software engineer specializing in full-stack development, distributed systems, and data engineering. Explore my educational journey."
+        />
+      </Head>
+      <TransitionEffect />
+      <main
+        className={`flex  w-full flex-col items-center justify-center dark:text-light`}
+      >
+        <Layout className="pt-16">
+          <Education
+            styleObj={{ marginTop: "calc(var(--base-font-size) * (32 / 32))" }}
+          />
+        </Layout>
+      </main>
+    </>
   );
 };
 
