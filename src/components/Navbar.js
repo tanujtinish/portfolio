a9cd5843231f6512fc09f1f800328acb929bfa67
tanujinish <tanujtinish@gmail.com>
2024-08-03 04:54:35 -0500

    updated portfolio - added glassmorphism header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -103,112 +103,116 @@ const Navbar = () => {
   }, [isOpen]);
 
   return (
-    <header
-      className="fixed w-full flex items-center justify-between font-medium z-10 
-    lg:px-16 z-1 md:px-12 sm:px-8 text-light bg-darkGlass dark:text-dark dark:bg-lightGlass
-    "
-      style={{
-        padding:
-          "calc(var(--base-font-size) * (24 / 32)) calc(var(--base-font-size) * (24 / 32))",
-        backdropFilter: "blur(calc(var(--base-font-size) * (24 / 32)))",
-        "-webkit-backdrop-filter":
-          "blur(calc(var(--base-font-size) * (24 / 32)))",
-      }}
-    >
-      <button
-        type="button"
-        className=" flex-col items-center justify-center hidden lg:flex"
-        aria-controls="mobile-menu"
-        aria-expanded={isOpen}
-        onClick={handleClick}
+    <>
+      {" "}
+      <header
+        className="fixed w-full flex items-center justify-between font-medium z-10 
+  lg:px-16 z-1 md:px-12 sm:px-8 text-light bg-darkGlass dark:text-dark dark:bg-lightGlass
+  "
         style={{
-          height: "calc(var(--base-font-size) * (36 / 32))",
-          width: "calc(var(--base-font-size) * (36 / 32))",
+          padding:
+            "calc(var(--base-font-size) * (24 / 32)) calc(var(--base-font-size) * (24 / 32))",
+          backdropFilter: "blur(calc(var(--base-font-size) * (24 / 32)))",
+          "-webkit-backdrop-filter":
+            "blur(calc(var(--base-font-size) * (24 / 32)))",
         }}
       >
-        <IoMenu size={"100%"} />
-      </button>
+        <button
+          type="button"
+          className=" flex-col items-center justify-center hidden lg:flex"
+          aria-controls="mobile-menu"
+          aria-expanded={isOpen}
+          onClick={handleClick}
+          style={{
+            height: "calc(var(--base-font-size) * (36 / 32))",
+            width: "calc(var(--base-font-size) * (36 / 32))",
+          }}
+        >
+          <IoMenu size={"100%"} />
+        </button>
 
-      <div
-        className="w-full flex justify-between items-center lg:hidden"
-        style={{ fontSize: "calc(var(--base-font-size) * (1.5 / 2))" }}
-      >
-        <nav className="flex items-center justify-center">
-          <CustomLink className="mr-4" href="/" title="Home" />
-          <CustomLink className="mx-4" href="/about" title="About" />
-          {/* <CustomLink className="mx-4" href="/projects" title="Projects" /> */}
-          {/* <CustomLink className="mx-4" href="/experience" title="Experience" />
-          <CustomLink className="mx-4" href="/education" title="Education" /> */}
-          <CustomLink className="mx-4" href="/skills" title="Skills" />
-          <CustomLink className="ml-4" href="/articles" title="Articles" />
-        </nav>
-        <nav
-          className="flex items-center justify-center flex-wrap lg:mt-2
-      "
+        <div
+          className="w-full flex justify-between items-center lg:hidden"
           style={{ fontSize: "calc(var(--base-font-size) * (1.5 / 2))" }}
         >
-          <motion.a
-            target={"_blank"}
-            className="mx-3"
-            href="https://github.com/tanujtinish"
-            whileHover={{ y: -2 }}
-            whileTap={{ scale: 0.9 }}
-            aria-label="Checkout my github profile"
+          <nav className="flex items-center justify-center">
+            <CustomLink className="mr-4" href="/" title="Home" />
+            <CustomLink className="mx-4" href="/about" title="About" />
+            {/* <CustomLink className="mx-4" href="/projects" title="Projects" /> */}
+            {/* <CustomLink className="mx-4" href="/experience" title="Experience" />
+        <CustomLink className="mx-4" href="/education" title="Education" /> */}
+            <CustomLink className="mx-4" href="/skills" title="Skills" />
+            <CustomLink className="ml-4" href="/articles" title="Articles" />
+          </nav>
+          <nav
+            className="flex items-center justify-center flex-wrap lg:mt-2
+    "
+            style={{ fontSize: "calc(var(--base-font-size) * (1.5 / 2))" }}
           >
-            <GithubIcon />
-          </motion.a>
-          <motion.a
-            target={"_blank"}
-            className="mx-3"
-            href="https://www.linkedin.com/in/tanuj-gupta-57b869168/"
-            whileHover={{ y: -2 }}
-            whileTap={{ scale: 0.9 }}
-            aria-label="Checkout my linkedin profile"
-          >
-            <LinkedInIcon />
-          </motion.a>
-          <motion.a
-            target={"_blank"}
-            className="mx-3"
-            href="https://leetcode.com/u/tanuj_gupta/"
-            whileHover={{ y: -2 }}
-            whileTap={{ scale: 0.9 }}
-            aria-label="Checkout my leetcode profile"
-          >
-            <LeetcodeIcon />
-          </motion.a>
-          <motion.a
-            target={"_blank"}
-            className="m-1 mr-3 sm:mx-1"
-            href="https://www.instagram.com/tanuj._.gupta/"
-            whileHover={{ y: -2 }}
-            whileTap={{ scale: 0.9 }}
-            aria-label="Checkout my Instagram profile"
-          >
-            <FaInstagram size="1em" color={mode === "dark" ? "pink" : ""} />
-          </motion.a>
+            <motion.a
+              target={"_blank"}
+              className="mx-3"
+              href="https://github.com/tanujtinish"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              aria-label="Checkout my github profile"
+            >
+              <GithubIcon />
+            </motion.a>
+            <motion.a
+              target={"_blank"}
+              className="mx-3"
+              href="https://www.linkedin.com/in/tanuj-gupta-57b869168/"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              aria-label="Checkout my linkedin profile"
+            >
+              <LinkedInIcon />
+            </motion.a>
+            <motion.a
+              target={"_blank"}
+              className="mx-3"
+              href="https://leetcode.com/u/tanuj_gupta/"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              aria-label="Checkout my leetcode profile"
+            >
+              <LeetcodeIcon />
+            </motion.a>
+            <motion.a
+              target={"_blank"}
+              className="m-1 mr-3 sm:mx-1"
+              href="https://www.instagram.com/tanuj._.gupta/"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              aria-label="Checkout my Instagram profile"
+            >
+              <FaInstagram size="1em" color={mode === "dark" ? "pink" : ""} />
+            </motion.a>
 
-          <button
-            onClick={() => setMode(mode === "light" ? "dark" : "light")}
-            className={`w-6 h-6 ease ml-3 flex items-center justify-center rounded-full p-1  
-            ${mode === "light" ? "bg-dark  text-light" : "bg-light  text-dark"}
-            `}
-            aria-label="theme-switcher"
-          >
-            {mode === "light" ? (
-              <SunIcon className={"fill-dark"} />
-            ) : (
-              <MoonIcon className={"fill-dark"} />
-            )}
-          </button>
-        </nav>
-      </div>
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={`w-6 h-6 ease ml-3 flex items-center justify-center rounded-full p-1  
+          ${mode === "light" ? "bg-dark  text-light" : "bg-light  text-dark"}
+          `}
+              aria-label="theme-switcher"
+            >
+              {mode === "light" ? (
+                <SunIcon className={"fill-dark"} />
+              ) : (
+                <MoonIcon className={"fill-dark"} />
+              )}
+            </button>
+          </nav>
+        </div>
+
+        <div className="absolute left-[50%] top-2 translate-x-[-50%] ">
+          {/* <Logo /> */}
+        </div>
+      </header>
       {isOpen ? (
         <motion.div
-          className=" flex justify-between items-center flex-col -translate-x-1/2
-      -translate-y-1/2
-       bg-dark/90 dark:bg-light/75 rounded-lg z-50 backdrop-blur-md
-      "
+          className=" flex justify-between items-center flex-col rounded-lg z-50 bg-darkGlass dark:bg-lightGlass rounded-lg z-50 backdrop-blur-md z-10 "
           initial={{ scale: 0, x: "-50%", y: "-50%", opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           style={{
@@ -241,11 +245,11 @@ const Navbar = () => {
               title="About"
             />
             {/* <CustomMobileLink
-              toggle={handleClick}
-              className=""
-              href="/projects"
-              title="Projects"
-            /> */}
+            toggle={handleClick}
+            className=""
+            href="/projects"
+            title="Projects"
+          /> */}
             <CustomMobileLink
               toggle={handleClick}
               className="ml-4 lg:m-0 lg:my-2"
@@ -253,17 +257,17 @@ const Navbar = () => {
               title="Articles"
             />
             {/* <CustomMobileLink
-              toggle={handleClick}
-              className=""
-              href="/experience"
-              title="Experience"
-            />
-            <CustomMobileLink
-              toggle={handleClick}
-              className=""
-              href="/education"
-              title="Education"
-            /> */}
+            toggle={handleClick}
+            className=""
+            href="/experience"
+            title="Experience"
+          />
+          <CustomMobileLink
+            toggle={handleClick}
+            className=""
+            href="/education"
+            title="Education"
+          /> */}
             <CustomMobileLink
               toggle={handleClick}
               className=""
@@ -273,7 +277,7 @@ const Navbar = () => {
           </nav>
           <nav
             className="flex items-center justify-center  mt-2
-      "
+    "
             style={{
               fontSize: "calc(var(--base-font-size) * 20/32)",
             }}
@@ -337,8 +341,8 @@ const Navbar = () => {
             <button
               onClick={() => setMode(mode === "light" ? "dark" : "light")}
               className={`ease m-1 ml-3 sm:mx-1 flex items-center justify-center rounded-full p-1  
-            ${mode === "light" ? "bg-dark  text-light" : "bg-light  text-dark"}
-            `}
+          ${mode === "light" ? "bg-dark  text-light" : "bg-light  text-dark"}
+          `}
               aria-label="theme-switcher"
               style={{
                 fontSize: "calc(var(--base-font-size) * 40/32)",
@@ -355,11 +359,7 @@ const Navbar = () => {
           </nav>
         </motion.div>
       ) : null}
-
-      <div className="absolute left-[50%] top-2 translate-x-[-50%] ">
-        {/* <Logo /> */}
-      </div>
-    </header>
+    </>
   );
 };
 
