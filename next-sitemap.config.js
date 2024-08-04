7bf49bf14596632969d3b10b8523e33383f8d211
tanujinish <tanujtinish@gmail.com>
2024-08-04 16:15:31 -0500

    updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  siteUrl: "https://portfolio-tanuj.vercel.app", // Replace with your own domain
+  siteUrl: "https://tanuj.org", // Replace with your own domain
   generateRobotsTxt: true, // (optional) Include robots.txt file
   exclude: ["/admin", "/drafts"], // (optional) Exclude specific routes
-  // Optional configuration to customize the generated sitemap.xml
-  // Some common options:
-  // 1. If you want to add additional paths to the sitemap, you can use the additionalPaths option:
-  // additionalPaths: ['/extra-page'],
-  // 2. If you want to provide a custom sitemapXml configuration:
-  // sitemapXml: { additionalNamespaces: { 'xhtml': 'http://www.w3.org/1999/xhtml' }, lastmod: new Date().toISOString(), }
+  additionalPaths: async (config) => {
+    return [
+      {
+        loc: "/skills", // This is the path you want to include
+        changefreq: "daily",
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      // You can add more paths if needed
+    ];
+  },
 };
