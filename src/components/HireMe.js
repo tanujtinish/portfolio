eac353ed62b367d3cb071a373aa2d3b8826ff7c5
tanujinish <tanujtinish@gmail.com>
2024-08-07 00:10:40 -0500

    updated portfolio

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -6,7 +6,7 @@ export const HireMe = () => {
   return (
     <div
       className="fixed left-4 bottom-4 flex flex-col items-center justify-center md:right-8 sm:right-0 
-    overflow-hidden md:bottom-auto md:left-auto md:top-0 md:absolute z-20"
+    overflow-hidden md:bottom-auto md:left-auto md:top-20 z-20"
     >
       <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
         <CircularText
